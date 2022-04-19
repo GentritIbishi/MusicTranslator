dd4de39f498dde10f112fd37021d6128eb79b364
Gentrit Ibishi <44057937+GentritIbishi@users.noreply.github.com>
2022-04-20 00:50:26 +0200

    Just send data from intent to LyricsActivity reminded and test API

---
+++ app/src/main/java/com/gentritibishi/musictranslator/LyricsActivity.java
@@ -0,0 +1,50 @@
+package com.gentritibishi.musictranslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+public class LyricsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    TextView tv_lyricsToSet;
+    Spinner spinner1;
+    String lyricsText = null;
+    String lyricsEncoded = null;
+    String sourceByDefault = "en";
+    String[] targetLanguage=new String[] {"en","it", "de", "es","fr","tr"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lyrics);
+
+        tv_lyricsToSet = findViewById(R.id.tv_lyricsToSet);
+        spinner1 = findViewById(R.id.spinner1);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(LyricsActivity.this, R.array.targets, R.layout.spinner_style);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String text = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+        //ktu na duhet me manipulu me seletimet nese eshte selektet english translate to english
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
